22be65f128bbf5a3c68d6ce92bd97ab5b97e16a8
sparkingsquirrel@gmail.com <sparkingsquirrel@gmail.com>
2025-06-13 12:53:54 -0500

    add get_file_content as part of feeding a directory to model

--- tests.py
+++ tests.py
@@ -1,6 +1,16 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
+
+def test():
+    result = get_file_content("calculator", "main.py")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
